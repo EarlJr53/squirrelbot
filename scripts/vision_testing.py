03d6afec51b963743ef23472a80b8ab3ea10646a
FunRobot <funrobo@brookem.dev>
2025-04-30 16:33:20 -0400

    adds new calibration images

--- scripts/vision_testing.py
+++ scripts/vision_testing.py
@@ -14,7 +14,7 @@ while True:
     ret, img = cap.read()
     cv2.imshow('Frame', img)
     if cv2.waitKey(1) & 0xFF == ord('c'):
-        cv2.imwrite('../media/images/'+str(num)+'jpg', img)
+        cv2.imwrite('../media/images/'+str(num)+'.jpg', img)
         print('Capture '+str(num)+' Successful!')
         num = num + 1
     if num == 4:
